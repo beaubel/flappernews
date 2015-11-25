6f92d96fafe2396384af74c407a2282a2bfe5b73
beaubel <Beau@Beaus-MacBook-Pro.local>
2015-11-25 22:30:55 +0100

    Posts Page
    De comments pagina toevoegen met up en downvotes

--- app.js
+++ app.js
@@ -3,22 +3,29 @@ app.config([
 '$stateProvider',
 '$urlRouterProvider',
 function($stateProvider,$urlRouterProvider) {
-	$stateProvider
-			.state('home', {
-				url: '/home',
-				templateUrl: '/home.html',
-				controller: 'MainCtrl'
 
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		})
+
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
 		});
 
-		$urlRouterProvider.otherwise('home');
-}]);
+	$urlRouterProvider.otherwise('home');
+}])
+
 app.factory('posts', [function(){
 	var o = {
-		posts: [{title:'hello', link:'', upvotes:0}]
+		posts: []
 	};
 	return o;
-}]);
+}])
 
 app.controller('MainCtrl', [
 '$scope',
@@ -33,7 +40,11 @@ function($scope, posts){
 		$scope.posts.push({
 			title: $scope.title,
 			link: $scope.link,
-			upvotes:1
+			upvotes:1,
+			comments: [
+				{author: 'Joe', body: 'Cool post', upvotes:0},
+				{author: 'Bob', body: 'No post', upvotes:0}
+			]
 		});
 		$scope.title='';
 		$scope.link='';
@@ -46,4 +57,23 @@ function($scope, posts){
 	$scope.decrementUpvotes = function(post) {
 		post.upvotes -=1;
 	};
+}])
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts) {
+	$scope.post = posts.posts[$stateParams.id];
+
+	$scope.addComment = function(){
+		if($scope.body === '') { return; }
+		$scope.post.comments.push({
+			body: $scope.body,
+			author: 'user',
+			upvotes: 0
+		});
+
+		$scope.body = '';
+};
 }]);
