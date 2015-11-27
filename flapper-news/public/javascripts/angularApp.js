e65214f3feb05cd257d635a8f92924a15a3a5beb
beaubel <Beau@Beaus-MacBook-Pro.local>
2015-11-27 14:17:34 +0100

    Laden van posts

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -8,7 +8,12 @@ function($stateProvider,$urlRouterProvider) {
 		.state('home', {
 			url: '/home',
 			templateUrl: '/home.html',
-			controller: 'MainCtrl'
+			controller: 'MainCtrl',
+			resolve: {
+				postPromise: ['posts', function(posts) {
+					return posts.getAll();
+				}]
+			}
 		})
 
 		.state('posts', {
